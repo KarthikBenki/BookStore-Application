988e87c9225c8fcf2f200091b388c229ae78fb99
KarthikBenki <karthikmc007@gmail.com>
2022-06-06 10:17:20 +0530

    Added find by id logic

--- src/main/java/com/bridgelabz/bookstoreapp/controller/UserController.java
+++ src/main/java/com/bridgelabz/bookstoreapp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.bookstoreapp.controller;
 import com.bridgelabz.bookstoreapp.dto.ResponseDTO;
 import com.bridgelabz.bookstoreapp.dto.UserDTO;
 import com.bridgelabz.bookstoreapp.dto.UserLoginDTO;
+import com.bridgelabz.bookstoreapp.entity.UserData;
 import com.bridgelabz.bookstoreapp.service.EmailSenderService;
 import com.bridgelabz.bookstoreapp.service.IUserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,6 @@ import javax.mail.MessagingException;
 public class UserController {
 
 
-
     @Autowired
     private EmailSenderService senderService;
 
@@ -25,32 +25,31 @@ public class UserController {
     private IUserRegistrationService userRegistrationService;
 
     /**
-     * @Purpose Testing the Api
      * @return A Welcome message to test the api
+     * @Purpose Testing the Api
      */
-    @GetMapping({"/welcome","/",""})
+    @GetMapping({"/welcome", "/", ""})
     public String welcomeApi() {
         return "Welcome To BookStore App";
     }
 
 
     /**
-     * @Purpose To check if the test mail is sending or not
      * @return status of email
+     * @Purpose To check if the test mail is sending or not
      */
     @GetMapping("/email/{toEmail}/{subject}/{body}")
     public ResponseEntity<ResponseDTO> sendTestEmail(@PathVariable String toEmail,
                                                      @PathVariable String subject,
-                                                     @PathVariable String body){
+                                                     @PathVariable String body) {
         senderService.sendEmail(toEmail,
                 subject,
                 body);
-        ResponseDTO responseDTO = new ResponseDTO("Sent Email Successfully",toEmail);
-        return new ResponseEntity<>(responseDTO,HttpStatus.OK);
+        ResponseDTO responseDTO = new ResponseDTO("Sent Email Successfully", toEmail);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     /**
-     *
      * @return status of email
      * @throws MessagingException
      */
@@ -60,43 +59,48 @@ public class UserController {
                 "This is subject",
                 "Hai Karthik Benki find the attachment below",
                 "C:\\Users\\karth\\Downloads\\download.jpg");
-        ResponseDTO responseDTO = new ResponseDTO("Sent Email Successfully","toEmail");
-        return new ResponseEntity<>(responseDTO,HttpStatus.OK);
+        ResponseDTO responseDTO = new ResponseDTO("Sent Email Successfully", "toEmail");
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
 
     /**
-     * @Purpose to register the user into the data base
      * @param userDTO
      * @return the status and user data
+     * @Purpose to register the user into the data base
      */
     @PostMapping("/register")
-    public ResponseEntity<ResponseDTO> registerUserInBookStore(@RequestBody UserDTO userDTO){
+    public ResponseEntity<ResponseDTO> registerUserInBookStore(@RequestBody UserDTO userDTO) {
         ResponseDTO responseDTO = userRegistrationService.registerUserInBookStore(userDTO);
-        return new ResponseEntity<>(responseDTO,HttpStatus.CREATED);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/verify/email/{otp}")
-    public ResponseEntity<ResponseDTO> verifyEmailUsingOtp(@PathVariable Long otp){
+    public ResponseEntity<ResponseDTO> verifyEmailUsingOtp(@PathVariable Long otp) {
         ResponseDTO responseDTO = userRegistrationService.verifyEmailUsingOtp(otp);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseDTO> loginUser(@RequestBody UserLoginDTO userLoginDTO){
+    public ResponseEntity<ResponseDTO> loginUser(@RequestBody UserLoginDTO userLoginDTO) {
         ResponseDTO responseDTO = userRegistrationService.loginUser(userLoginDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ResponseDTO> deleteUserById(@PathVariable(value = "id") long id){
+    public ResponseEntity<ResponseDTO> deleteUserById(@PathVariable(value = "id") long id) {
         String email = userRegistrationService.deleteUserById(id);
-        ResponseDTO responseDTO = new ResponseDTO("The data with " +id +" is deleted and the deleted is ",email);
+        ResponseDTO responseDTO = new ResponseDTO("The data with " + id + " is deleted and the deleted is ", email);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
-
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ResponseDTO> findUserById(@PathVariable(value = "id") long id) {
+        UserData user = userRegistrationService.findUserById(id);
+        ResponseDTO responseDTO = new ResponseDTO("The user found successfully", user);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
 
 
 }
